9ff9031d4ab73de87126c6203661c4e5f56bf032
Andrew Breckenridge <asbreckenridge@me.com>
2016-08-20 12:32:51 -0700

    create Molecule framework

---
+++ Molecule/Molecule.h
@@ -0,0 +1,19 @@
+//
+//  Molecule.h
+//  Molecule
+//
+//  Created by Andrew on 8/20/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Molecule.
+FOUNDATION_EXPORT double MoleculeVersionNumber;
+
+//! Project version string for Molecule.
+FOUNDATION_EXPORT const unsigned char MoleculeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Molecule/PublicHeader.h>
+
+
